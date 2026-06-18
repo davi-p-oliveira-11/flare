8883c87e31981a25bdad38c564ddd1bb5515cb21
Davi P. Oliveira <dpo.dev11@gmail.com>
2026-06-18 18:53:36 -0300

    feat(client): implement authentication flow structure

---
+++ app/(auth)/login.tsx
@@ -0,0 +1,66 @@
+import { COLORS } from "@/constants/theme";
+import { styles } from "@/styles/auth.styles";
+import { useSSO } from "@clerk/clerk-expo";
+import { Ionicons } from "@expo/vector-icons";
+import { useRouter } from "expo-router";
+import { Image, Text, TouchableOpacity, View } from "react-native";
+
+export default function login() {
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  const { startSSOFlow } = useSSO();
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  const router = useRouter();
+
+  const handleGoogleSignIn = async () => {
+    try {
+      const { createdSessionId, setActive } = await startSSOFlow({
+        strategy: "oauth_google",
+      });
+
+      if (setActive && createdSessionId) {
+        setActive({ session: createdSessionId });
+        router.replace("/(tabs)");
+      }
+    } catch (error) {
+      console.error("OAuth error:", error);
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.brandSection}>
+        <View style={styles.logoContainer}>
+          <Ionicons name="flame" size={32} color={COLORS.primary} />
+        </View>
+        <Text style={styles.appName}>Flare</Text>
+        <Text style={styles.tagline}>get in touch with everything</Text>
+      </View>
+
+      <View style={styles.illustrationContainer}>
+        <Image
+          source={require("../../assets/images/auth.image.png")}
+          style={styles.illustration}
+          resizeMode="cover"
+        />
+      </View>
+
+      {/* LOGIN SECTION */}
+      <View style={styles.loginSection}>
+        <TouchableOpacity
+          style={styles.googleButton}
+          onPress={handleGoogleSignIn}
+          activeOpacity={0.9}
+        >
+          <View style={styles.googleIconContainer}>
+            <Ionicons name="logo-google" size={20} color={COLORS.surface} />
+          </View>
+          <Text style={styles.googleButtonText}>Continue with Google</Text>
+        </TouchableOpacity>
+
+        <Text style={styles.termsText}>
+          By continuing, you agree to our Terms and Privacy Policy
+        </Text>
+      </View>
+    </View>
+  );
+}
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,12 +1,24 @@
-import { Stack } from "expo-router";
+import { tokenCache } from "@/cache";
+import InitialLayout from "@/components/InitialLayout";
+import { ClerkLoaded, ClerkProvider } from "@clerk/clerk-expo";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
+
+const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!;
+
+if (!publishableKey) {
+  throw new Error("Add your Clerk Publishable Key to the .env file");
+}
 
 export default function RootLayout() {
   return (
-    <SafeAreaProvider>
-      <SafeAreaView style={{ flex: 1, backgroundColor: "#000" }}>
-        <Stack screenOptions={{ headerShown: false }} />
-      </SafeAreaView>
-    </SafeAreaProvider>
+    <ClerkProvider tokenCache={tokenCache} publishableKey={publishableKey}>
+      <ClerkLoaded>
+        <SafeAreaProvider>
+          <SafeAreaView style={{ flex: 1, backgroundColor: "#000" }}>
+            <InitialLayout />
+          </SafeAreaView>
+        </SafeAreaProvider>
+      </ClerkLoaded>
+    </ClerkProvider>
   );
 }
